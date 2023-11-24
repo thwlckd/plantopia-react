b7c0a5da3fa7169e5ff55b2cdc940cddce5e9e06
thwlckd <thwlckd@naver.com>
2023-11-24 23:21:54 +0900

    Feat: OpenWeatherMap api 로딩 상태 스켈레톤 기능 추가 - react-loading-skeleton 사용

--- src/pages/mainPage/WeatherSection.tsx
+++ src/pages/mainPage/WeatherSection.tsx
@@ -1,17 +1,15 @@
 import { useCallback } from 'react';
 import { weatherContents } from '@/constants/weather';
+import Skeleton from 'react-loading-skeleton';
 
 import LOCATION from '@/assets/images/icons/location.png';
 import { useWeatherData } from '@/hooks';
 
 const WeatherSection = () => {
-  const {
-    data: weatherInfo,
-    // isLoading,
-  } = useWeatherData();
+  const { data: weatherInfo, isLoading } = useWeatherData();
 
   const formatTemperature = useCallback(
-    (temp: number) => `${Math.floor(temp)}°`,
+    (temp: number | undefined) => temp && `${Math.floor(temp)}°`,
     [],
   );
 
@@ -23,30 +21,60 @@ const WeatherSection = () => {
 
   const content = getWeatherContent(weatherInfo?.data.weather[0].id);
 
-  return weatherInfo && content ? (
+  return isLoading ? (
+    <WeatherSkeleton />
+  ) : (
     <div className="weather_wrapper">
       <div className="text_wrapper">
         <div className="location_wrapper">
           <img src={LOCATION} className="weather_icon" alt="location" />
-          <span className="text">{weatherInfo.data.name}</span>
+          <span className="text">{weatherInfo?.data.name}</span>
         </div>
         <div className="weather_text_box temperature_wrapper">
           <span className="text_lg">
-            {content.title} {formatTemperature(weatherInfo.data.main.temp)}
+            {content?.title} {formatTemperature(weatherInfo?.data.main.temp)}
           </span>
           <span className="text_sm">
-            {formatTemperature(weatherInfo.data.main.temp_max)}
+            {formatTemperature(weatherInfo?.data.main.temp_max)}
           </span>
           <span className="text_sm">
-            {formatTemperature(weatherInfo.data.main.temp_min)}
+            {formatTemperature(weatherInfo?.data.main.temp_min)}
           </span>
         </div>
-        <div className="weather_text_box">{content.description}</div>
+        <div className="weather_text_box">{content?.description}</div>
       </div>
-      <img src={content.imgSrc} className="weather_icon" alt="weather" />
+      <img src={content?.imgSrc} className="weather_icon" alt="weather" />
     </div>
-  ) : (
-    <div>loading..</div>
+  );
+};
+
+const WeatherSkeleton = () => {
+  return (
+    <div className="weather_wrapper">
+      <div className="text_wrapper">
+        <Skeleton
+          containerClassName="line_skeleton"
+          width="5rem"
+          height="1.5rem"
+        />
+        <Skeleton
+          containerClassName="line_skeleton"
+          width="10rem"
+          height="1.5rem"
+        />
+        <Skeleton
+          containerClassName="line_skeleton"
+          width="15rem"
+          height="1.5rem"
+        />
+      </div>
+      <Skeleton
+        className="img_skeleton"
+        width="7rem"
+        height="7rem"
+        borderRadius="50%"
+      />
+    </div>
   );
 };
 
